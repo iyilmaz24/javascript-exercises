23c3abf1c382fc87dd7db44e8456cb40441acdf9
Irfan Yilmaz <iy.usa@outlook.com>
2023-10-15 18:08:18 -0400

    finish exercise, use test file to check solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    
+    let output = "";
 
+    for(let i = 0; i < num; i++){
+        output += string;
+    }
+    if(num < 0){return 'ERROR'};
+    return output;
 };
 
 // Do not edit below this line
