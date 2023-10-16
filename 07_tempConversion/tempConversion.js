33801b27181b340997f9fb6dd6243bdef5df9427
Irfan Yilmaz <iy.usa@outlook.com>
2023-10-15 22:27:52 -0400

    finish exercise, check solution with tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  return (parseFloat(((num - 32) / 1.8).toFixed(1)));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  return (parseFloat(((num * 1.8) + 32).toFixed(1)));
 };
 
 // Do not edit below this line
