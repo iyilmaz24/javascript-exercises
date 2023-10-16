0a3bfa2346a6fa9beddd108408601b962666a362
Irfan Yilmaz <iy.usa@outlook.com>
2023-10-15 21:28:45 -0400

    complete exercise, check solution with tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,12 @@
-const reverseString = function() {
+const reverseString = function(inputString) {
+
+    let returnString = [];
+
+    for(let i = inputString.length;  i !== 0 ; i--){
+        returnString.push(inputString[i-1]);
+    }
+    
+    return (returnString.join(""));
 
 };
 
