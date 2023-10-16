6f7c93fb59bacc69b2de3f7d821e390ff3e95cb5
Irfan Yilmaz <iy.usa@outlook.com>
2023-10-15 21:37:39 -0400

    complete exercise, check solution with tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
 
+    returnArray = array.filter((item) => !args.includes(item));
+
+    return returnArray;
 };
 
 // Do not edit below this line
