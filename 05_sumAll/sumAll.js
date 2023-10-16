50a83a94738a54da11cd3bb3bb49ae70a62914cb
Irfan Yilmaz <iy.usa@outlook.com>
2023-10-15 21:59:06 -0400

    finish exercise, check solution with tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+
+    if(!(Number.isInteger(num1) && Number.isInteger(num2)) || !(num1 >= 0 && num2 >= 0)){
+        return "ERROR";
+    }
+
+    if(num1 < num2){
+        startNum = num1;
+        endNum = num2; 
+    }
+    else{   
+        startNum = num2;
+        endNum = num1;
+    }
+
+    let total = 0;
+    for(let i = startNum; i < endNum; i++){
+        total += i;
+    }
+    return total + endNum;
 
 };
 
