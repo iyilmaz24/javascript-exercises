1ebbeee2897853e79835c859c7f1e1f2cdac2eaa
Irfan Yilmaz <iy.usa@outlook.com>
2023-10-22 16:24:43 -0400

    finish exercise, check solution with tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arrArg){
+  result = arrArg.reduce( (sum, curr) => sum += curr, 0)  
+  return result
 };
 
-const multiply = function() {
-
+const multiply = function(arrArg) {
+  result = arrArg.reduce( (sum, curr) => sum *= curr, 1)
+  return result
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if(a === 0 || a === 1){
+    return 1
+  }
+  return a * factorial(a - 1)
 };
 
 // Do not edit below this line
