4202554884c66333fedabe82b58f745ccd879bb4
Irfan Yilmaz <iy.usa@outlook.com>
2023-10-23 03:28:07 -0400

    finish excerise, check solutions with tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
 
-};
+
+const fibonacci = function(count){
+
+    if (count < 0) return "OOPS";
+    if (count === 0) return 0;
+
+    num1st = 1
+    num2nd = 0
+
+    for(let i = 2; i <= count; i++){
+        let current = num1st + num2nd
+        num2nd = num1st
+        num1st = current
+    }
+    return num1st
+}
+
 
 // Do not edit below this line
 module.exports = fibonacci;
